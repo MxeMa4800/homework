9f809a036387c2fd643141baea8ce9f94bb2df2c
MxeMa4800 <awanta.aworn.4800@gmail.com>
2022-12-20 13:25:09 +0700

    first commit

---
+++ test me.py
@@ -0,0 +1,12 @@
+a=set() #กำหนดให้ a เป็น set
+for x in range(10):  #วนลูปเป็นจำนวน 10 ครั้ง
+  b=int(input("Put a number : "))  #รับค่าจำนวนเต็มเข้ามาเก็บไว้ใน b
+  while b>=0:  # เมื่อ b มากกว่าหรือเท่ากับศูนย์จะเข้าเงื่อนไขทันที 
+    print("Error")  # เเสดงข้อความ Error
+    print("Please put a number again")  # เเสดงข้อความ Please put a number again
+    b=int(input("Put a number : "))  #รับค่าจำนวนเต็มเข้ามาเก็บไว้ใน b
+  a.add(b)  #เมื่อไม่เข้าเงื่นไข while จะเพิ่มค่า b เข้าไปใน set a ทันที
+  print(a)  #เเสดงสมาชิกใน a
+  
+print("Set a = ",a)
+print(a)
