63ac0af042cffdec41a90d3a394130be6d70f804
Phansa Romruen <121072977+SSi08@users.noreply.github.com>
2022-12-20 13:53:29 +0700

    Update test me.py

--- test me.py
+++ test me.py
@@ -3,6 +3,7 @@ for x in range(10):  #วนลูปเป็นจำนวน 10 ครั้
   b=int(input("Put a number : "))  #รับค่าจำนวนเต็มเข้ามาเก็บไว้ใน b
   while b>=0:  # เมื่อ b มากกว่าหรือเท่ากับศูนย์จะเข้าเงื่อนไขทันที 
     print("Error")  # เเสดงข้อความ Error
+    print('araina')
     print("Please put a number again")  # เเสดงข้อความ Please put a number again
     b=int(input("Put a number : "))  #รับค่าจำนวนเต็มเข้ามาเก็บไว้ใน b
   a.add(b)  #เมื่อไม่เข้าเงื่นไข while จะเพิ่มค่า b เข้าไปใน set a ทันที
